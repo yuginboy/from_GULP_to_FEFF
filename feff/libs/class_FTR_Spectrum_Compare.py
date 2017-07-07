154801d6b67d4cffa0e69fc1896e8145e5a76910
yuginboy <preved*NIX2016>
2017-07-07 13:49:08 +0200

    add implementations to apply two models fit

--- feff/libs/class_FTR_Spectrum_Compare.py
+++ feff/libs/class_FTR_Spectrum_Compare.py
@@ -1329,6 +1329,9 @@ if __name__ == '__main__':
     a.scale_theory_factor_FTR = 0.81
     a.scale_experiment_factor_FTR = 1.0
 
+    a.model_A.numberOfSerialEquivalentAtoms = 2
+    a.model_B.numberOfSerialEquivalentAtoms = 3
+
     #  change the user name, which parameters for xftf transformation you want to use:
     a.user = 'PK'
     # change tha sample preparation method:
