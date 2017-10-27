f1e70b481bda1f102dcbdaee201e7451f1a64d91
yuginboy <preved*NIX2016>
2017-10-27 17:32:57 +0200

    add filter for a selecting models when we want to calc the mix of two models

--- feff/libs/class_for_parallel_comparison.py
+++ feff/libs/class_for_parallel_comparison.py
@@ -1201,6 +1201,7 @@ class FTR_gulp_to_feff_A_model_base():
         print('{}'.format(self.minimum.snapshotName))
         runtime = timer() - start
         print("runtime is {0:f} seconds".format(runtime))
+        plt.close()
 
         # # store table to ASCII file:
         # self.table.outDirPath = self.outMinValsDir
